7fbe0c782fa2435dbc70278be3f69357c1211ca1
Cobrayyxx <muhammadhazim86@gmail.com>
2021-08-20 21:55:44 +0700

    do some settings and trying dummyapp

--- dummy_app/views.py
+++ dummy_app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 import requests, json 
 # Create your views here.
 def dummy(request):
-    return(request, 'index.html')
+    return render(request, 'index.html')
